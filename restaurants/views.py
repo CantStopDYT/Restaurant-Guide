f7f541f84f9760239ddcdd996d50945785d42ba5
David Fan <fand.kanade@gmail.com>
2020-04-05 11:48:05 -0400

    Added distance querying to API

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework_gis.filters import DistanceToPointFilter
 
 from restaurants.models import Restaurant, Location
 from restaurants.serializers import RestaurantSerializer, LocationGeoSerializer
@@ -9,6 +10,7 @@ class LocationList(viewsets.ReadOnlyModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationGeoSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = (DistanceToPointFilter, )
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
